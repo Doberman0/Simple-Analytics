7c76b958efdc7869c78d70efde27bd8d873f2651
Dabeer <DabeerM@hotmail.com>
2020-06-24 17:20:31 +0100

    Total amount of commissions generated that day

--- app.py
+++ app.py
@@ -20,6 +20,14 @@ def toDate(date_string):
 	except ValueError:
 		raise ValueError('{} is not valid date in the format YYYY-MM-DD'.format(date_string))
 
+def totalOrderForDay(orders_in_a_day: Dict[str, List[float]]) -> float:
+	'''
+	orders_in_a_day is a dictionary structured as such: 
+	{order_id: [total_amount per order line associated with order_id]}
+	Returns {order_id: total amount per order_id}
+	'''
+	return {order_id:sum(orders_in_a_day[order_id]) for order_id in orders_in_a_day}
+
 def calculateAverageOrderTotalForDay(orders_in_a_day: Dict[str, List[float]]) -> float:
 	'''
 	orders_in_a_day is a dictionary structured as such: 
@@ -49,6 +57,9 @@ def controller():
 	total_discount = 0
 	number_of_discounts = 0 # same as total number of items ordered
 	total_discount_rate = 0 # Total amount ordered that day (is it per item?)
+	#To calculate the total amount of commission 
+	orders_vendors = {} # Key:order_id, val:[vendor_id]
+	total_commission = 0
 	
 	# Get all the orders placed that day
 	orders_csv = pd.read_csv('data/orders.csv')
@@ -56,7 +67,19 @@ def controller():
 		if orders_csv.iloc[row]['created_at'][:10] == str(date):
 			order_ids.add(orders_csv.iloc[row]['id'])
 			unique_customers_set.add(orders_csv.iloc[row]['customer_id'])
-	
+			# To check commissions
+			if orders_csv.iloc[row]['id'] not in orders_vendors:
+				orders_vendors[orders_csv.iloc[row]['id']] = orders_csv.iloc[row]['vendor_id']
+			else:
+				orders_vendors[orders_csv.iloc[row]['id']] += orders_csv.iloc[row]['vendor_id']
+
+	# Get the commission rates
+	commissions_csv = pd.read_csv('data/commissions.csv')
+	commission_rates = [0 for _ in range(10)] # index is the vendor_id-1
+	for row in range(commissions_csv.shape[0]):
+		if commissions_csv.iloc[row]['date'] == str(date):
+			commission_rates[commissions_csv.iloc[row]['vendor_id'] - 1] = commissions_csv.iloc[row]['rate']  
+
 	# Getting the numbers of items sold that day
 	total_num_items = 0
 	order_lines_csv = pd.read_csv('data/order_lines.csv')
@@ -79,12 +102,19 @@ def controller():
 				orders_in_a_day[order_id] = row_total_amount
 			else:
 				orders_in_a_day[order_id] = orders_in_a_day[order_id] + row_total_amount
+
+	# Find the total amount of commissions
+	# I assume it's total amount (per order) * commission rate
+	total_amounts = totalOrderForDay(orders_in_a_day)
+	for order_id in total_amounts:
+		total_commission += commission_rates[orders_vendors[order_id] - 1] * total_amounts[order_id] 
 	
 	response = jsonify({'total_num_items': str(total_num_items),
 	 'total number of customers who ordered': len(unique_customers_set),
 	 'Total discount': total_discount,
 	 'Average discount': total_discount_rate/number_of_discounts,
-	 'Average Total in a day': calculateAverageOrderTotalForDay(orders_in_a_day)})
+	 'Average Total in a day': calculateAverageOrderTotalForDay(orders_in_a_day),
+	 'Total commission for the day': total_commission})
 	
 	return response, 200
 
